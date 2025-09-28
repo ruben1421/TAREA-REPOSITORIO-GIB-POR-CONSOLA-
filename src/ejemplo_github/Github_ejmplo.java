5cd5f8debb62a555c1f1bf611b4c32e4cb0a1908
ruben1421 <rubenantoniomunoz0@gmail.com>
2025-09-28 18:45:46 +0200

    Cambios Alumno3
    Adios Mundo

--- src/ejemplo_github/Github_ejmplo.java
+++ src/ejemplo_github/Github_ejmplo.java
@@ -5,7 +5,7 @@ public class Github_ejmplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 System.out.println("¡Hola, Mundo!");
+		 System.out.println("¡Adios, Mundo!");
 		 
 		 System.out.println("Como estan");
 	}
